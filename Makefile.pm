883601767654edec78905555376ef039a54accd9
Slaven Rezic <slaven@rezic.de>
2002-03-06 20:38:22 +0000

    with Tk::Canvas support

--- Makefile.pm
+++ Makefile.pm
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: Makefile.pm,v 1.2 2002/03/06 01:30:28 eserte Exp $
+# $Id: Makefile.pm,v 1.3 2002/03/06 20:38:22 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2002 Slaven Rezic. All rights reserved.
@@ -48,7 +48,7 @@ sub _generate {
 	$seen{$target}++;
 	return;
     }
-    my @depends = _all_depends($make_target);
+    my @depends = _all_depends($self->{Make}, $make_target);
     if (!@depends) {
 	$seen{$target}++;
 	warn "No depends for target $target";
@@ -67,7 +67,7 @@ sub _generate {
 }
 
 sub _all_depends {
-    my($make_target) = @_;
+    my($make, $make_target) = @_;
     my @depends;
     if ($make_target->colon) {
 	push @depends, $make_target->colon->depend;
@@ -76,14 +76,60 @@ sub _all_depends {
 	    push @depends, $rule->depend;
 	}
     }
-    @depends;
+    map { split(/\s+/,$make->subsvars($_)) } @depends;
+}
+
+package GraphViz;
+
+sub as_tk_canvas {
+    my($self, $c) = @_;
+    GraphViz::TkCanvas::graphviz($c, $self->as_plain);
+}
+
+package GraphViz::TkCanvas;
+use Text::ParseWords;
+sub graphviz {
+    my($c, $text) = @_;
+
+    my $tfm = sub { my($x,$y) = @_; ($x*100,$y*100) };
+    foreach my $l (split /\n/, $text) {
+	my(@w) = shellwords($l);
+	if ($w[0] eq 'graph') {
+	    $c->configure(-scrollregion => [$tfm->(0,0),$tfm->($w[2],$w[3])]);
+	} elsif ($w[0] eq 'node') {
+	    my($x,$y) = $tfm->($w[2],$w[3]);
+	    my($w,$h) = $tfm->($w[4],$w[5]);
+	    my $text = $w[6];
+	    $c->createOval($x-$w/2,$y-$h/2,$x+$w/2,$y+$h/2);
+	    $c->createText($x,$y,-text => $text);
+	} elsif ($w[0] eq 'edge') {
+	    my $no = $w[3];
+	    my @coords;
+	    for(my $i=0; $i<$no*2; $i+=2) {
+		push @coords, $tfm->($w[4+$i], $w[5+$i]);
+	    }
+	    $c->createLine(@coords, -arrow => "last", -smooth => 1);
+	} else {
+	    warn "?@w\n";
+	}
+    }
 }
 
 package main;
 
-my $gm = GraphViz::Makefile->new(undef, "Makefile");
-$gm->generate;
-open(O, ">/tmp/bla.ps") or die $!;
-binmode O;
-print O $gm->{GraphViz}->as_ps;
-close O;
+use Getopt::Long;
+my $file = "Makefile";
+GetOptions("f|file=s" => \$file) or die;
+use Tk;
+my $mw = new MainWindow;
+my $c = $mw->Scrolled("Canvas")->pack(-fill=>"both",-expand=>1);
+my $rule = shift || "all";
+my $gm = GraphViz::Makefile->new(undef, $file);
+$gm->generate($rule);
+#open(O, ">/tmp/bla.fig") or die $!;
+#binmode O;
+#print O
+ $gm->{GraphViz}->as_tk_canvas($c);
+#close O;
+
+MainLoop;
