6a758b4c4592c11fb7d1373555fff4f7dc656833
Slaven Rezic <slaven@rezic.de>
2002-03-07 22:31:07 +0000

    crude external make handling

--- Makefile.pm
+++ Makefile.pm
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: Makefile.pm,v 1.3 2002/03/06 20:38:22 eserte Exp $
+# $Id: Makefile.pm,v 1.4 2002/03/07 22:31:07 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2002 Slaven Rezic. All rights reserved.
@@ -13,23 +13,26 @@
 # WWW:  http://www.rezic.de/eserte/
 #
 
-# TODO: multiple makefiles, includes...
-
 package GraphViz::Makefile;
 use GraphViz;
 use Make;
+use strict;
+
+my $V = 1;
 
 sub new {
-    my($pkg, $g, $make) = @_;
+    my($pkg, $g, $make, $prefix) = @_;
     $g = GraphViz->new unless $g;
     if (!$make) {
 	$make = Make->new;
     } elsif (!UNIVERSAL::isa($make, "Make")) {
 	$make = Make->new(Makefile => $make);
     }
-    bless { GraphViz => $g,
-	    Make     => $make
-	  };
+    my $self = { GraphViz => $g,
+		 Make     => $make,
+		 Prefix   => ($prefix||""),
+	       };
+    bless $self, $pkg;
 }
 
 sub generate {
@@ -41,42 +44,168 @@ sub generate {
 
 sub _generate {
     my($self, $target, $seen) = @_;
-    return if $seen{$target};
+    return if $seen->{$target};
     my $make_target = $self->{Make}->Target($target);
     if (!$make_target) {
-	warn "Can't get make target for $target";
-	$seen{$target}++;
+	warn "Can't get make target for $target\n" if $V;
+	$seen->{$target}++;
 	return;
     }
-    my @depends = _all_depends($self->{Make}, $make_target);
+    my @depends = $self->_all_depends($self->{Make}, $make_target);
     if (!@depends) {
-	$seen{$target}++;
-	warn "No depends for target $target";
+	$seen->{$target}++;
+	warn "No depends for target $target\n" if $V;
 	return;
     }
     my $g = $self->{GraphViz};
-    $g->add_node($target);
+    my $prefix = $self->{Prefix};
+    $g->add_node("$prefix$target");
     foreach my $dep (@depends) {
-	$g->add_node($dep) unless $seen{$dep};
-	$g->add_edge($target, $dep);
+	$g->add_node("$prefix$dep") unless $seen->{$dep};
+	$g->add_edge("$prefix$target", "$prefix$dep");
+warn "$prefix$target => $prefix$dep\n";
     }
-    $seen{$target}++;
+    $seen->{$target}++;
     foreach my $dep (@depends) {
 	$self->_generate($dep, $seen);
     }
 }
 
+sub guess_external_makes {
+    my($self, $make_rule, $cmd) = @_;
+    if ($cmd =~ /\bcd\s+(\w+)\s*;\s*make\s+(\w+)/) {
+	my($dir, $rule) = ($1, $2); # XXX parse nothing as "all"
+	warn "dir: $dir, rule: $rule\n";
+	my $f = "$dir/makefile"; # XXX make better. use $make->{GNU}
+	$f = "$dir/Makefile" if !-r $f;
+	my $gm2 = GraphViz::Makefile->new($self->{GraphViz}, $f, "$dir/"); # XXX save_pwd verwenden; -f option auswerten
+	$gm2->generate($rule);
+
+	$self->{GraphViz}->add_edge($make_rule->Name, "$dir/$rule");
+
+    }
+}
+
 sub _all_depends {
-    my($make, $make_target) = @_;
+    my($self, $make, $make_target) = @_;
     my @depends;
     if ($make_target->colon) {
-	push @depends, $make_target->colon->depend;
+#	push @depends, $make_target->colon->depend;
+	push @depends, $make_target->colon->exp_depend;
+	$self->guess_external_makes($make_target, $make_target->colon->exp_command);
     } elsif ($make_target->dcolon) {
 	foreach my $rule ($make_target->dcolon) {
-	    push @depends, $rule->depend;
+	    #push @depends, $rule->depend;
+	    push @depends, $rule->exp_depend;
+	    $self->guess_external_makes($rule, $rule->exp_command);
 	}
     }
-    map { split(/\s+/,$make->subsvars($_)) } @depends;
+#    map { split(/\s+/,$make->subsvars($_)) } @depends;
+    @depends;
+}
+
+{
+package Make;
+
+sub subsvars
+{
+ my $self = shift;
+ local $_ = shift;
+ my @var = @_;
+ push(@var,$self->{Override},$self->{Vars},\%ENV);
+ croak("Trying to subsitute undef value") unless (defined $_); 
+ while (/(?<!\$)\$\(([^()]+)\)/ || /(?<!\$)\$([<\@^?*])/)
+  {
+   my ($key,$head,$tail) = ($1,$`,$');
+   my $value;
+   if ($key =~ /^([\w._]+|\S)(?::(.*))?$/)
+    {
+     my ($var,$op) = ($1,$2);
+#warn "key=$key var=$var op=$op vars=@var\n";
+     foreach my $hash (@var)
+      {
+       $value = $hash->{$var};
+       if (defined $value)
+        {
+         last; 
+        }
+      }
+     unless (defined $value)
+      {
+#XXX $@ not defined?
+#XXX       die "$var not defined in '$_'" unless (length($var) > 1); 
+       $value = '';
+      }
+     if (defined $op)
+      {
+       if ($op =~ /^s(.).*\1.*\1/)
+        {
+         local $_ = $self->subsvars($value);
+         $op =~ s/\\/\\\\/g;
+         eval $op.'g';
+         $value = $_;
+        }
+       else
+        {
+         die "$var:$op = '$value'\n"; 
+        }   
+      }
+    }
+   elsif ($key =~ /wildcard\s*(.*)$/)
+    {
+     $value = join(' ',glob($self->pathname($1)));
+    }
+   elsif ($key =~ /shell\s*(.*)$/)
+    {
+     $value = join(' ',split('\n',`$1`));
+    }
+   elsif ($key =~ /addprefix\s*([^,]*),(.*)$/)
+    {
+     $value = join(' ',map($1 . $_,split('\s+',$2)));
+    }
+   elsif ($key =~ /notdir\s*(.*)$/)
+    {
+     my @files = split(/\s+/,$1);
+     foreach (@files)
+      {
+       s#^.*/([^/]*)$#$1#;
+      }
+     $value = join(' ',@files);
+    }
+   elsif ($key =~ /dir\s*(.*)$/)
+    {
+     my @files = split(/\s+/,$1);
+     foreach (@files)
+      {
+       s#^(.*)/[^/]*$#$1#;
+      }
+     $value = join(' ',@files);
+    }
+   elsif ($key =~ /^subst\s+([^,]*),([^,]*),(.*)$/)
+    {
+     my ($a,$b) = ($1,$2);
+     $value = $3;
+     $a =~ s/\./\\./;
+     $value =~ s/$a/$b/; 
+    }
+   elsif ($key =~ /^mktmp,(\S+)\s*(.*)$/)
+    {
+     my ($file,$content) = ($1,$2);
+     open(TMP,">$file") || die "Cannot open $file:$!";
+     $content =~ s/\\n//g;
+     print TMP $content;
+     close(TMP);
+     $value = $file;
+    }
+   else
+    {
+     warn "Cannot evaluate '$key' in '$_'\n";
+    }
+   $_ = "$head$value$tail";
+  }
+ s/\$\$/\$/g;
+ return $_;
+}
 }
 
 package GraphViz;
@@ -100,8 +229,8 @@ sub graphviz {
 	    my($x,$y) = $tfm->($w[2],$w[3]);
 	    my($w,$h) = $tfm->($w[4],$w[5]);
 	    my $text = $w[6];
-	    $c->createOval($x-$w/2,$y-$h/2,$x+$w/2,$y+$h/2);
-	    $c->createText($x,$y,-text => $text);
+	    $c->createOval($x-$w/2,$y-$h/2,$x+$w/2,$y+$h/2, -fill=>"gray");
+	    $c->createText($x,$y,-text => $text, -tag => ["rule","rule_$text"]);
 	} elsif ($w[0] eq 'edge') {
 	    my $no = $w[3];
 	    my @coords;
@@ -117,10 +246,13 @@ sub graphviz {
 
 package main;
 
+$ENV{MAKE}="make";
 use Getopt::Long;
 my $file = "Makefile";
 GetOptions("f|file=s" => \$file) or die;
 use Tk;
+use lib "/home/e/eserte/src/bbbike/lib";
+use Tk::CanvasUtil;
 my $mw = new MainWindow;
 my $c = $mw->Scrolled("Canvas")->pack(-fill=>"both",-expand=>1);
 my $rule = shift || "all";
@@ -130,6 +262,83 @@ $gm->generate($rule);
 #binmode O;
 #print O
  $gm->{GraphViz}->as_tk_canvas($c);
+
+my @c = $c->coords("rule_$rule");
+$c->see($c[0],$c[1]);
+#  $c->bind("rule", "<1>" => sub {
+#  	     my $c = shift;
+#  	     my $file = ($c->gettags("current"))[1];
+#  	     if (is_in_path("emacsclient")) {
+#  		 system('emacsclient', '--no-wait', $file);
+#  	     }
+#  	 });
+
 #close O;
 
 MainLoop;
+
+# REPO BEGIN
+# REPO NAME is_in_path /home/e/eserte/src/repository 
+# REPO MD5 1b42243230d92021e6c361e37c9771d1
+
+=head2 is_in_path($prog)
+
+=for category File
+
+Return the pathname of $prog, if the program is in the PATH, or undef
+otherwise.
+
+DEPENDENCY: file_name_is_absolute
+
+=cut
+
+sub is_in_path {
+    my($prog) = @_;
+    return $prog if (file_name_is_absolute($prog) and -f $prog and -x $prog);
+    require Config;
+    my $sep = $Config::Config{'path_sep'} || ':';
+    foreach (split(/$sep/o, $ENV{PATH})) {
+	if ($^O eq 'MSWin32') {
+	    return "$_\\$prog"
+		if (-x "$_\\$prog.bat" ||
+		    -x "$_\\$prog.com" ||
+		    -x "$_\\$prog.exe");
+	} else {
+	    return "$_/$prog" if (-x "$_/$prog");
+	}
+    }
+    undef;
+}
+# REPO END
+
+# REPO BEGIN
+# REPO NAME file_name_is_absolute /home/e/eserte/src/repository 
+# REPO MD5 a77759517bc00f13c52bb91d861d07d0
+
+=head2 file_name_is_absolute($file)
+
+=for category File
+
+Return true, if supplied file name is absolute. This is only necessary
+for older perls where File::Spec is not part of the system.
+
+=cut
+
+sub file_name_is_absolute {
+    my $file = shift;
+    my $r;
+    eval {
+        require File::Spec;
+        $r = File::Spec->file_name_is_absolute($file);
+    };
+    if ($@) {
+	if ($^O eq 'MSWin32') {
+	    $r = ($file =~ m;^([a-z]:(/|\\)|\\\\|//);i);
+	} else {
+	    $r = ($file =~ m|^/|);
+	}
+    }
+    $r;
+}
+# REPO END
+
