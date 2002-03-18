51ecf47c691a09ec4733890ad4f6f559fdab5fc3
Slaven Rezic <slaven@rezic.de>
2002-03-18 14:09:27 +0000

    removed script code, Tk code etc.

--- Makefile.pm
+++ Makefile.pm
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: Makefile.pm,v 1.6 2002/03/18 14:03:37 eserte Exp $
+# $Id: Makefile.pm,v 1.7 2002/03/18 14:09:27 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2002 Slaven Rezic. All rights reserved.
@@ -18,7 +18,10 @@ use GraphViz;
 use Make;
 use strict;
 
-my $V = 1;
+use vars qw($VERSION $V);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.7 $ =~ /(\d+)\.(\d+)/);
+
+$V = 1 unless defined $V;
 
 sub new {
     my($pkg, $g, $make, $prefix) = @_;
@@ -63,7 +66,7 @@ sub _generate {
     foreach my $dep (@depends) {
 	$g->add_node("$prefix$dep") unless $seen->{$dep};
 	$g->add_edge("$prefix$target", "$prefix$dep");
-warn "$prefix$target => $prefix$dep\n";
+#warn "$prefix$target => $prefix$dep\n";
     }
     $seen->{$target}++;
     foreach my $dep (@depends) {
@@ -93,7 +96,7 @@ sub guess_external_makes {
 	    }
 	}
 
-	warn "dir: $dir, file: $makefile, rule: $rule\n";
+#	warn "dir: $dir, file: $makefile, rule: $rule\n";
 	my $f = "$dir/$makefile"; # XXX make better. use $make->{GNU}
 	$f = "$dir/Makefile" if !-r $f;
 	my $gm2 = GraphViz::Makefile->new($self->{GraphViz}, $f, "$dir/"); # XXX save_pwd verwenden; -f option auswerten
@@ -124,7 +127,8 @@ sub _all_depends {
 }
 
 {
-package Make;
+package
+    Make;
 
 sub subsvars
 {
@@ -140,7 +144,6 @@ sub subsvars
    if ($key =~ /^([\w._]+|\S)(?::(.*))?$/)
     {
      my ($var,$op) = ($1,$2);
-#warn "key=$key var=$var op=$op vars=@var\n";
      foreach my $hash (@var)
       {
        $value = $hash->{$var};
@@ -227,142 +230,4 @@ sub subsvars
 }
 }
 
-package GraphViz;
-
-sub as_tk_canvas {
-    my($self, $c) = @_;
-    GraphViz::TkCanvas::graphviz($c, $self->as_plain);
-}
-
-package GraphViz::TkCanvas;
-use Text::ParseWords;
-sub graphviz {
-    my($c, $text) = @_;
-
-    my $tfm = sub { my($x,$y) = @_; ($x*100,$y*100) };
-    foreach my $l (split /\n/, $text) {
-	my(@w) = shellwords($l);
-	if ($w[0] eq 'graph') {
-	    $c->configure(-scrollregion => [$tfm->(0,0),$tfm->($w[2],$w[3])]);
-	} elsif ($w[0] eq 'node') {
-	    my($x,$y) = $tfm->($w[2],$w[3]);
-	    my($w,$h) = $tfm->($w[4],$w[5]);
-	    my $text = $w[6];
-	    $c->createOval($x-$w/2,$y-$h/2,$x+$w/2,$y+$h/2, -fill=>"gray");
-	    $c->createText($x,$y,-text => $text, -tag => ["rule","rule_$text"]);
-	} elsif ($w[0] eq 'edge') {
-	    my $no = $w[3];
-	    my @coords;
-	    for(my $i=0; $i<$no*2; $i+=2) {
-		push @coords, $tfm->($w[4+$i], $w[5+$i]);
-	    }
-	    $c->createLine(@coords, -arrow => "last", -smooth => 1);
-	} else {
-	    warn "?@w\n";
-	}
-    }
-}
-
-package main;
-
-$ENV{MAKE}="make";
-use Getopt::Long;
-my $file = "Makefile";
-my $outputtype = "tkcanvas";
-GetOptions("f|file=s" => \$file,
-	   "T=s" => \$outputtype) or die;
-
-my $rule = shift || "all";
-my $gm = GraphViz::Makefile->new(undef, $file);
-$gm->generate($rule);
-
-if ($outputtype eq 'tkcanvas') {
-    use Tk; # XXX no use!
-    use lib "/home/e/eserte/src/bbbike/lib";
-    use Tk::CanvasUtil;
-    my $mw = new MainWindow;
-    my $c = $mw->Scrolled("Canvas")->pack(-fill=>"both",-expand=>1);
-    $gm->{GraphViz}->as_tk_canvas($c);
-    my @c = $c->coords("rule_$rule");
-    $c->see($c[0],$c[1]);
-#  $c->bind("rule", "<1>" => sub {
-#  	     my $c = shift;
-#  	     my $file = ($c->gettags("current"))[1];
-#  	     if (is_in_path("emacsclient")) {
-#  		 system('emacsclient', '--no-wait', $file);
-#  	     }
-#  	 });
-    MainLoop;
-} else {
-    my $meth = "as_" . $outputtype;
-    open(O, ">/tmp/bla.$outputtype") or die $!;
-    binmode O;
-    print O $gm->{GraphViz}->$meth();
-    close O;
-}
-
-# REPO BEGIN
-# REPO NAME is_in_path /home/e/eserte/src/repository 
-# REPO MD5 1b42243230d92021e6c361e37c9771d1
-
-=head2 is_in_path($prog)
-
-=for category File
-
-Return the pathname of $prog, if the program is in the PATH, or undef
-otherwise.
-
-DEPENDENCY: file_name_is_absolute
-
-=cut
-
-sub is_in_path {
-    my($prog) = @_;
-    return $prog if (file_name_is_absolute($prog) and -f $prog and -x $prog);
-    require Config;
-    my $sep = $Config::Config{'path_sep'} || ':';
-    foreach (split(/$sep/o, $ENV{PATH})) {
-	if ($^O eq 'MSWin32') {
-	    return "$_\\$prog"
-		if (-x "$_\\$prog.bat" ||
-		    -x "$_\\$prog.com" ||
-		    -x "$_\\$prog.exe");
-	} else {
-	    return "$_/$prog" if (-x "$_/$prog");
-	}
-    }
-    undef;
-}
-# REPO END
-
-# REPO BEGIN
-# REPO NAME file_name_is_absolute /home/e/eserte/src/repository 
-# REPO MD5 a77759517bc00f13c52bb91d861d07d0
-
-=head2 file_name_is_absolute($file)
-
-=for category File
-
-Return true, if supplied file name is absolute. This is only necessary
-for older perls where File::Spec is not part of the system.
-
-=cut
-
-sub file_name_is_absolute {
-    my $file = shift;
-    my $r;
-    eval {
-        require File::Spec;
-        $r = File::Spec->file_name_is_absolute($file);
-    };
-    if ($@) {
-	if ($^O eq 'MSWin32') {
-	    $r = ($file =~ m;^([a-z]:(/|\\)|\\\\|//);i);
-	} else {
-	    $r = ($file =~ m|^/|);
-	}
-    }
-    $r;
-}
-# REPO END
-
+1;
